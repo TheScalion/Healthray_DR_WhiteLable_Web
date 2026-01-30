3b19837768fa9e99837df06675dcec17f45f98b5
Tirth <tirth@Healthray.com>
2026-01-30 16:35:47 +0530

    Initial commit
    Generated by react-native@0.83.1

---
+++ android/app/src/main/java/com/heritage_for_doctor/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.heritage_for_doctor
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Heritage_for_Doctor"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
